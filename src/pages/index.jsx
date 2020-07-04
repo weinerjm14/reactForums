4fdc8d7598936e25c02315cd0effaee7d2c38a1e
Moses Cosme <mosesmaxcosme@gmail.com>
2020-07-04 03:36:23 -0500

    changed eslint rules

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,23 +3,23 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className='container'>
       <Head>
         <title>reactForums test</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
-        <h1 className="title">
+        <h1 className='title'>
           This is
           {' '}
-          <a href="https://nextjs.org">reactForums!</a>
+          <a href='https://nextjs.org'>reactForums!</a>
         </h1>
 
-        <div className="grid">
+        <div className='grid'>
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
+            href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+            className='card'
           >
             <h3>Deploy &rarr;</h3>
             <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
@@ -29,13 +29,13 @@ export default function Home() {
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Powered by
           {' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          <img src='/vercel.svg' alt='Vercel Logo' className='logo' />
         </a>
       </footer>
 
